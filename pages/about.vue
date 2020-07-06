89943cd21f1b0e8b59eaa303405985a241415bd7
Marina Egereva <marina.egereva@gmail.com>
2020-07-06 21:57:29 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, vel, voluptatum! Amet assumenda dolores earum in obcaecati praesentium quae voluptatum?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, vel, voluptatum! Amet assumenda dolores earum in obcaecati praesentium quae voluptatum?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+  </section>
+</template>
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+</style>
+
